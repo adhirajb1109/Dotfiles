1bb5782638ca42d9f82fb8212a3a993d00a6d4f7
AdhirajBhatia <74768134+AdhirajBhatia@users.noreply.github.com>
2021-10-26 10:08:55 +0530

    Update and rename .hyper.js to hyper/.hyper.js

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -10,7 +10,7 @@ module.exports = {
         // default font size in pixels for all tabs
         fontSize: 16,
         // font family with optional fallbacks
-        fontFamily: 'Hurmit Nerd Font',
+        fontFamily: 'SpaceMono Nerd Font',
         // default font weight: 'normal' or 'bold'
         fontWeight: 'normal',
         // font weight for bold characters: 'normal' or 'bold'
